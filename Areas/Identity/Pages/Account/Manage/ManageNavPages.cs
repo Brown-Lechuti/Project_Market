d4d5dd838ddf8757fd6dfb85a344553d98bc76b8
BrownLechuti <brownlechuti@gmail.com>
2021-11-19 18:37:20 +0200

    Progress: addupload photo page

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -21,6 +21,7 @@ namespace Project_Market.Areas.Identity.Pages.Account.Manage
         public static string ExternalLogins => "ExternalLogins";
 
         public static string PersonalData => "PersonalData";
+        public static string UploadPhotos => "UploadPhotos";
 
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
@@ -37,6 +38,7 @@ namespace Project_Market.Areas.Identity.Pages.Account.Manage
         public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
 
         public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
+        public static string UploadPhotosNavClass(ViewContext viewContext) => PageNavClass(viewContext, UploadPhotos);
 
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
 
