01edda2a12a43e99df1a7736d83c27d88bb6e600
BrownLechuti <brownlechuti@gmail.com>
2021-11-20 23:35:28 +0200

    Control image view access to owners only

--- Areas/Identity/Pages/Account/Manage/UploadFileModel.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/UploadFileModel.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,6 +13,9 @@ namespace Project_Market.Areas.Identity.Pages.Account.Manage
 {
     public class UploadFileModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
         private IHostingEnvironment _environment;
         private readonly IWebHostEnvironment webHostEnvironment;
 
@@ -26,12 +30,24 @@ namespace Project_Market.Areas.Identity.Pages.Account.Manage
         public IFormFile Upload { get; set; }
         public async Task OnPostAsync()
         {
-            var file = Path.Combine(webHostEnvironment.WebRootPath, "images", Upload.FileName);
-            using (var fileStream = new FileStream(file, FileMode.Create))
+
+            if (Upload != null)
             {
-                await Upload.CopyToAsync(fileStream);
-                Status = "Your image has been uploaded!";
+
+                var fileName = User.Identity.Name + Guid.NewGuid().ToString() + "_" + Path.GetFileName(Upload.FileName);
+
+                var imagePath = Path.Combine(webHostEnvironment.WebRootPath, "images", fileName);
+                using (var fileStream = new FileStream(imagePath, FileMode.Create))
+                {
+                    await Upload.CopyToAsync(fileStream);
+                    Status = "Your image has been uploaded!";
+                }
             }
+            else
+            {
+                Status = "Please upload valid image file.";
+            }
+
 
             
         }
