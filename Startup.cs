776231deabaa97e5bb21de2e6e6850664909b5c8
BrownLechuti <brownlechuti@gmail.com>
2021-11-23 00:06:11 +0200

    Close

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Blobs;
+using Azure.Core.Extensions;
 /*            services.AddDbContext<ApplicationDbContext>
                 (options =>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
                 sqlServerOptionsAction: sqlOptions =>
@@ -36,6 +40,11 @@ namespace Project_Market
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddAzureClients(builder =>
+            {
+                builder.AddBlobServiceClient(Configuration["DefaultEndpointsProtocol=https;AccountName=storebbl;AccountKey=LCC4STdXqimJmE2JpmD1QbKnXARGHAkVimaAyy8odI3gxo4sncLG6f4hm7Grn5695liG6bpX7Fv7UPA67qzvdQ==;EndpointSuffix=core.windows.net:blob"], preferMsi: true);
+                builder.AddQueueServiceClient(Configuration["DefaultEndpointsProtocol=https;AccountName=storebbl;AccountKey=LCC4STdXqimJmE2JpmD1QbKnXARGHAkVimaAyy8odI3gxo4sncLG6f4hm7Grn5695liG6bpX7Fv7UPA67qzvdQ==;EndpointSuffix=core.windows.net:queue"], preferMsi: true);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -65,6 +74,31 @@ namespace Project_Market
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+    }
+    internal static class StartupExtensions
+    {
+        public static IAzureClientBuilder<BlobServiceClient, BlobClientOptions> AddBlobServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
+        {
+            if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
+            {
+                return builder.AddBlobServiceClient(serviceUri);
+            }
+            else
+            {
+                return builder.AddBlobServiceClient(serviceUriOrConnectionString);
+            }
+        }
+        public static IAzureClientBuilder<QueueServiceClient, QueueClientOptions> AddQueueServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
+        {
+            if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
+            {
+                return builder.AddQueueServiceClient(serviceUri);
+            }
+            else
+            {
+                return builder.AddQueueServiceClient(serviceUriOrConnectionString);
+            }
         }
     }
 }
